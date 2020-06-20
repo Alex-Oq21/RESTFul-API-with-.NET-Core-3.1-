04dbe3d465f0d2a67e095036bb549ce65c1fb856
Alexis Cárdenas <alexiscardednas85@hotmail.com>
2020-06-19 19:51:24 -0500

    Para evitar ataques de Overposting se creo un DTO con las prropiedades de la identidad Post y se configuro usando Automapper.

--- SocialMedia/SocialMedia.Api/Controllers/PostController.cs
+++ SocialMedia/SocialMedia.Api/Controllers/PostController.cs
@@ -1,7 +1,11 @@
-﻿ using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.DTOs;
 using SocialMedia.Core.Entities;
 using SocialMedia.Core.Interfaces;
 using SocialMedia.Infrastructure.Repositories;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SocialMedia.Api.Controllers
@@ -11,25 +15,31 @@ namespace SocialMedia.Api.Controllers
     public class PostController : ControllerBase
     {
         private readonly IPostRepository _postRepository;
-        public PostController(IPostRepository postRepository)
+        private readonly IMapper _mapper;
+        public PostController(IPostRepository postRepository, IMapper mapper)
         {
             _postRepository = postRepository;
+            _mapper = mapper;
         }
         [HttpGet]
         public  async Task <IActionResult> GetPosts()
         {
             var posts = await _postRepository.GetPosts();
-            return Ok(posts);
+            var PostsDTO = _mapper.Map<IEnumerable<PostDTO>>(posts);
+           
+            return Ok(PostsDTO);
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> GetPost(int id)
         {
             var post = await _postRepository.GetPost(id);
+            var postDTO = _mapper.Map<PostDTO>(post);
             return Ok(post);
         }
         [HttpPost]
-        public async Task<IActionResult>Post(Post post)
+        public async Task<IActionResult>Post(PostDTO postDTO)
         {
+            var post = _mapper.Map<Post>(postDTO);
             await _postRepository.InsertPost(post);
             return Ok(post);
         }
